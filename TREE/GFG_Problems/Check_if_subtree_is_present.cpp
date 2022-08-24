76c81b9b1aacfb1ce1f872b394a08d0df6852ba8
shubham-begn <108439580+shubham-begn@users.noreply.github.com>
2022-08-24 19:34:47 +0530

    Create Check_if_subtree_is_present.cpp

---
+++ TREE/GFG_Problems/Check_if_subtree_is_present.cpp
@@ -0,0 +1,22 @@
+ bool identical(Node*t,Node*s)
+    {
+        if(t==NULL && s==NULL) return true;
+        if((t==NULL && s!=NULL)|| (t!=NULL &&s==NULL)) return false;
+        else if(t!=NULL && s!=NULL){
+        if((t->data==s->data)&& identical(t->left,s->left) && identical(t->right,s->right))
+        return true;
+        
+        return false;
+        }
+        return false;
+    }
+    bool isSubTree(Node* T, Node* S) 
+    {
+       if( S==NULL) return true;
+       if(T==NULL) return false;
+       
+       if(identical(T,S)) return true;
+       return isSubTree(T->left,S)|| isSubTree(T->right,S);
+        
+        
+    }
