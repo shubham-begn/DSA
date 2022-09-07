44504564f6175add453fa8c7a3604850f54931d4
shubham-begn <108439580+shubham-begn@users.noreply.github.com>
2022-09-07 23:27:18 +0530

    Create Minimum in rotated sorted array.cpp

---
+++ Array/Minimum in rotated sorted array.cpp
@@ -0,0 +1,12 @@
+   int findMin(vector<int>& nums) {
+        int low=0,high=nums.size()-1;
+        while(low< high)
+        {
+            int mid=(low+high)/2;
+           if(nums[high]<nums[mid])
+               low=mid+1;
+            else high=mid;
+            
+        }
+        return nums[high];
+    }
